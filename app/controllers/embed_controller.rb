5a3be0c5d8724232a2d8652bc8a35ad214f2d9b3
Pedro Garcia <pedro@simbiotica.es>
2014-05-08 10:19:43 +0200

    testing print map

--- app/controllers/embed_controller.rb
+++ app/controllers/embed_controller.rb
@@ -1,6 +1,13 @@
 class EmbedController < ApplicationController
   layout 'embed'
 
+  def map
+    if params[:print] == 'map'
+      @map_url = request.original_url.sub('print=map', '')
+      render :map_print 
+    end
+  end
+
   # GET /embed/country/:id
   def countries_show
     country = Api::Country.find_by_iso(params[:id])['countries'][0]
